e2db3658224d9f16d3024adc914c4f8bcbcd8671
vgl <vgl@edi.tail04e59.ts.net>
2024-11-10 00:05:33 +0300

    monodetr decoder

--- vjepa_depth.py
+++ vjepa_depth.py
@@ -16,81 +16,87 @@ from evals.video_classification_frozen.utils import (
 import numpy as np
 import warnings
 import os
-from utils import Video3DBBoxDataset, get_dataloaders
+# from utils import Video3DBBoxDataset, get_dataloaders
 import decord
 import cv2
-from MonoDETR.lib.models.monodetr.depthaware_transformer import build_depthaware_transformer
+import sys
+import sys
+import os
 
-resolution = 224
-device = 'cuda'
-pretrained_path= '/home/vgl/emir/weights/v-jepa_weights/vitl16.pth.tar'
-model_name = 'vit_large'
-patch_size = 16
-tubelet_size = 2
-frame_step = 4
-frames_per_clip =  16
-uniform_power = True
-checkpoint_key = 'target_encoder'
-use_SiLU = False
-tight_SiLU = False
-use_sdpa = False
-attend_accross_segments = True
-batch = 8
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'MonoDETR')))
+print(os.path.abspath(os.path.join(os.path.dirname(__file__), 'MonoDETR')))
+
+from lib.helpers.model_helper import build_model
+# resolution = 224
+# device = 'cuda'
+# pretrained_path= '/home/vgl/emir/weights/v-jepa_weights/vitl16.pth.tar'
+# model_name = 'vit_large'
+# patch_size = 16
+# tubelet_size = 2
+# frame_step = 4
+# frames_per_clip =  16
+# uniform_power = True
+# checkpoint_key = 'target_encoder'
+# use_SiLU = False
+# tight_SiLU = False
+# use_sdpa = False
+# attend_accross_segments = True
+# batch = 8
 
 
-def main():
-    depth_encoder = init_model(
-        crop_size=resolution,
-        device=device,
-        pretrained=pretrained_path,
-        model_name=model_name,
-        patch_size=patch_size,
-        tubelet_size=tubelet_size,
-        frames_per_clip=frames_per_clip,
-        uniform_power=uniform_power,
-        checkpoint_key=checkpoint_key,
-        use_SiLU=use_SiLU,
-        tight_SiLU=tight_SiLU,
-        use_sdpa=use_sdpa,)
-    video_paths = [
-    "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000001/input.mp4",
-    "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000002/input.mp4",
-    "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000003/input.mp4",
-    ]
-    K_jsons = [
-        "/home/vgl/emir/datasets/tudl/train_real/000001/scene_camera.json",
-        "/home/vgl/emir/datasets/tudl/train_real/000002/scene_camera.json",
-        "/home/vgl/emir/datasets/tudl/train_real/000003/scene_camera.json"
-    ]
-    gt_jsons = [
-        "/home/vgl/emir/datasets/tudl/train_real/000001/scene_gt.json",
-        "/home/vgl/emir/datasets/tudl/train_real/000002/scene_gt.json",
-        "/home/vgl/emir/datasets/tudl/train_real/000003/scene_gt.json"
-    ]
-    models = [
-        "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000001.ply",
-        "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000002.ply",
-        "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000003.ply"
-    ]
-    decord.bridge.set_bridge('torch')
-    # vd = Video3DBBoxDataset(
-    #     video_paths=video_paths,
-    #     frame_step=frame_step,
-    #     K_jsons=K_jsons,
-    #     gt_jsons=gt_jsons,
-    #     models=models,
-    #     frames_per_clip=frames_per_clip,
-    # )
-    # print(bbox[0]['rotation_matrix'].shape)
-    build_depthaware_transformer()
-    # train_loader, val_loader = get_dataloaders(vd, 8)
-    # for batch in train_loader:
-    #     x, bbox = batch
-    #     print(bbox['rotation_matrices'].shape)
-    #     print(bbox['intrinsics'].shape)
-    #     print(bbox['dimensions'].shape)
-    #     print(bbox['centers'].shape)
-    #     break
+# def main():
+#     depth_encoder = init_model(
+#         crop_size=resolution,
+#         device=device,
+#         pretrained=pretrained_path,
+#         model_name=model_name,
+#         patch_size=patch_size,
+#         tubelet_size=tubelet_size,
+#         frames_per_clip=frames_per_clip,
+#         uniform_power=uniform_power,
+#         checkpoint_key=checkpoint_key,
+#         use_SiLU=use_SiLU,
+#         tight_SiLU=tight_SiLU,
+#         use_sdpa=use_sdpa,)
+#     video_paths = [
+#     "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000001/input.mp4",
+#     "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000002/input.mp4",
+#     "/home/vgl/emir/datasets/tudl/train_real/compressed_objects/segmentation_videos/000003/input.mp4",
+#     ]
+#     K_jsons = [
+#         "/home/vgl/emir/datasets/tudl/train_real/000001/scene_camera.json",
+#         "/home/vgl/emir/datasets/tudl/train_real/000002/scene_camera.json",
+#         "/home/vgl/emir/datasets/tudl/train_real/000003/scene_camera.json"
+#     ]
+#     gt_jsons = [
+#         "/home/vgl/emir/datasets/tudl/train_real/000001/scene_gt.json",
+#         "/home/vgl/emir/datasets/tudl/train_real/000002/scene_gt.json",
+#         "/home/vgl/emir/datasets/tudl/train_real/000003/scene_gt.json"
+#     ]
+#     models = [
+#         "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000001.ply",
+#         "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000002.ply",
+#         "/home/vgl/emir/datasets/tudl/tudl_models/models/obj_000003.ply"
+#     ]
+#     decord.bridge.set_bridge('torch')
+#     # vd = Video3DBBoxDataset(
+#     #     video_paths=video_paths,
+#     #     frame_step=frame_step,
+#     #     K_jsons=K_jsons,
+#     #     gt_jsons=gt_jsons,
+#     #     models=models,
+#     #     frames_per_clip=frames_per_clip,
+#     # )
+#     # print(bbox[0]['rotation_matrix'].shape)
+#     build_depthaware_transformer()
+#     # train_loader, val_loader = get_dataloaders(vd, 8)
+#     # for batch in train_loader:
+#     #     x, bbox = batch
+#     #     print(bbox['rotation_matrices'].shape)
+#     #     print(bbox['intrinsics'].shape)
+#     #     print(bbox['dimensions'].shape)
+#     #     print(bbox['centers'].shape)
+#     #     break
     
     
     
@@ -102,5 +108,5 @@ def main():
 
         
         
-if __name__ == '__main__':
-    main()   
+# if __name__ == '__main__':
+#     main()   
